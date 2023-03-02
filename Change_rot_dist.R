ce54c38c6599fe69f10f3b3aec08b2df2c409014
kujawa.21 <kujawa.21@OH15GYBS3>
2023-03-02 12:46:39 -0500

    Add script 'change_rot_dist'

---
+++ Change_rot_dist.R
@@ -0,0 +1,23 @@
+### rebuild distribution of scenarios to add a cover crop
+
+Change_rot_dist <- function(rotations,cc_rot, cc_rate) {
+  
+  
+  
+  
+  
+  
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
