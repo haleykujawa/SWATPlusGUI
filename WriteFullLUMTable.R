720b81dacd1da3bdd91eb16ee9918a154ff77e05
kujawa.21 <kujawa.21@OH15GYBS3>
2023-03-09 14:52:37 -0500

    Write full LUM table
    Works for every combo of mgt x tile x buffers x gww. Still in testing if it introduced any errors in the model, but it functions as intended.

--- WriteFullLUMTable.R
+++ WriteFullLUMTable.R
@@ -1,111 +1,112 @@
 ##### Rewrite landuse.lum file so every rotation has an option to add tile, grassed waterways, and vegtative buffers ##########
 rm(list = ls())
 library(here)
+library(stringr)
 ### Set up data frame with known inputs ####
 # 7 rotations, CS_FT, CS_RT, CS_RotT, CS_NT, CS_NTcc, CSWS, CSWcc
 # and the reverse SC_FT, SC_RT, SC_RotT, SC_NT, SC_NTcc
 
-here("Baseline")
+
 
 ####### Build base data frame-- no tile, no buffers, no gww ##############
-DF<-data.frame(matrix(nrow=12, ncol=14))
-colnames(DF)<-c("name", "cal_group", "plnt_com",  "mgt",  "cn2",  "cons_prac",  "urban",            
+basedf<-data.frame(matrix(nrow=12, ncol=14))
+colnames(basedf)<-c("name", "cal_group", "plnt_com",  "mgt",  "cn2",  "cons_prac",  "urban",            
                   "urb_ro",   "ov_mann",  "tile",   "sep",  "vfs",  "grww", "bmp")
 
-DF$name<-c("CS_FT", "CS_RT", "CS_RotT", "CS_NT", "CS_NTcc", "CSWS", "CSWcc","SC_FT", "SC_RT", "SC_RotT", "SC_NT", "SC_NTcc")
+basedf$name<-c("CS_FT", "CS_RT", "CS_RotT", "CS_NT", "CS_NTcc", "CSWS", "CSWcc","SC_FT", "SC_RT", "SC_RotT", "SC_NT", "SC_NTcc")
 
-DF$cal_group<-"null"
+basedf$cal_group<-"null"
 
 #plant community
-DF$plnt_com[grepl("CS",DF$name)]<-"CS_comm"
-DF$plnt_com[grepl("SC",DF$name)]<-"SC_comm"
-DF$plnt_com[grepl("CS_NTcc",DF$name)]<-"CSR_comm"
-DF$plnt_com[grepl("SC_NTcc",DF$name)]<-"SCR_comm" #Check that this is right and not causing an issue, I think it may need to be SRC
-DF$plnt_com[grepl("CSWS",DF$name)]<-"CSWS_comm"
-DF$plnt_com[grepl("CSWcc",DF$name)]<-"CSWR_comm"
+basedf$plnt_com[grepl("CS",basedf$name)]<-"CS_comm"
+basedf$plnt_com[grepl("SC",basedf$name)]<-"SC_comm"
+basedf$plnt_com[grepl("CS_NTcc",basedf$name)]<-"CSR_comm"
+basedf$plnt_com[grepl("SC_NTcc",basedf$name)]<-"SCR_comm" #Check that this is right and not causing an issue, I think it may need to be SRC
+basedf$plnt_com[grepl("CSWS",basedf$name)]<-"CSWS_comm"
+basedf$plnt_com[grepl("CSWcc",basedf$name)]<-"CSWR_comm"
 
 #management
-DF$mgt[grepl("CS_FT",DF$name)]<-"CS_FullTill"
-DF$mgt[grepl("SC_FT",DF$name)]<-"SC_FullTill"
-DF$mgt[grepl("CS_RT",DF$name)]<-"CS_ReduceTill"
-DF$mgt[grepl("SC_RT",DF$name)]<-"SC_ReduceTill"
-DF$mgt[grepl("CS_RotT",DF$name)]<-"CS_RotNoTill"
-DF$mgt[grepl("SC_RotT",DF$name)]<-"SC_RotNoTill"
-DF$mgt[grepl("CS_NT",DF$name)]<-"CS_NoTill"
-DF$mgt[grepl("SC_NT",DF$name)]<-"SC_NoTill"
-DF$mgt[grepl("CS_NTcc",DF$name)]<-"CS_NoTillCC"
-DF$mgt[grepl("SC_NTcc",DF$name)]<-"SC_NoTillCC"
-DF$mgt[grepl("CSWS",DF$name)]<-"CSWS"
-DF$mgt[grepl("CSWcc",DF$name)]<-"CSW_CC"
+basedf$mgt[grepl("CS_FT",basedf$name)]<-"CS_FullTill"
+basedf$mgt[grepl("SC_FT",basedf$name)]<-"SC_FullTill"
+basedf$mgt[grepl("CS_RT",basedf$name)]<-"CS_ReduceTill"
+basedf$mgt[grepl("SC_RT",basedf$name)]<-"SC_ReduceTill"
+basedf$mgt[grepl("CS_RotT",basedf$name)]<-"CS_RotNoTill"
+basedf$mgt[grepl("SC_RotT",basedf$name)]<-"SC_RotNoTill"
+basedf$mgt[grepl("CS_NT",basedf$name)]<-"CS_NoTill"
+basedf$mgt[grepl("SC_NT",basedf$name)]<-"SC_NoTill"
+basedf$mgt[grepl("CS_NTcc",basedf$name)]<-"CS_NoTillCC"
+basedf$mgt[grepl("SC_NTcc",basedf$name)]<-"SC_NoTillCC"
+basedf$mgt[grepl("CSWS",basedf$name)]<-"CSWS"
+basedf$mgt[grepl("CSWcc",basedf$name)]<-"CSW_CC"
 
 #cn2
-DF$cn2[grepl("CS_FT",DF$name)]<-"rc_strow_p"
-DF$cn2[grepl("SC_FT",DF$name)]<-"rc_strow_p"
-DF$cn2[grepl("CS_RT",DF$name)]<-"rc_strow_g"
-DF$cn2[grepl("SC_RT",DF$name)]<-"rc_strow_g"
-DF$cn2[grepl("CS_RotT",DF$name)]<-"rc_strow_g"
-DF$cn2[grepl("SC_RotT",DF$name)]<-"rc_strow_g"
-DF$cn2[grepl("CS_NT",DF$name)]<-"rc_strowres_g"
-DF$cn2[grepl("SC_NT",DF$name)]<-"rc_strowres_g"
-DF$cn2[grepl("CS_NTcc",DF$name)]<-"rc_strowres_g"
-DF$cn2[grepl("SC_NTcc",DF$name)]<-"rc_strowres_g"
-DF$cn2[grepl("CSWS",DF$name)]<-"rc_strowres_g"
-DF$cn2[grepl("CSWcc",DF$name)]<-"rc_strowres_g"
+basedf$cn2[grepl("CS_FT",basedf$name)]<-"rc_strow_p"
+basedf$cn2[grepl("SC_FT",basedf$name)]<-"rc_strow_p"
+basedf$cn2[grepl("CS_RT",basedf$name)]<-"rc_strow_g"
+basedf$cn2[grepl("SC_RT",basedf$name)]<-"rc_strow_g"
+basedf$cn2[grepl("CS_RotT",basedf$name)]<-"rc_strow_g"
+basedf$cn2[grepl("SC_RotT",basedf$name)]<-"rc_strow_g"
+basedf$cn2[grepl("CS_NT",basedf$name)]<-"rc_strowres_g"
+basedf$cn2[grepl("SC_NT",basedf$name)]<-"rc_strowres_g"
+basedf$cn2[grepl("CS_NTcc",basedf$name)]<-"rc_strowres_g"
+basedf$cn2[grepl("SC_NTcc",basedf$name)]<-"rc_strowres_g"
+basedf$cn2[grepl("CSWS",basedf$name)]<-"rc_strowres_g"
+basedf$cn2[grepl("CSWcc",basedf$name)]<-"rc_strowres_g"
 
 #cons prac
-DF$cons_prac<-"up_down_slope"
+basedf$cons_prac<-"up_down_slope"
 
 #urban
-DF$urban<-"null"
-DF$urb_ro<-"null"
+basedf$urban<-"null"
+basedf$urb_ro<-"null"
 
 #ov_mann
-DF$ov_mann[grepl("CS_FT",DF$name)]<-"convtill_nores"
-DF$ov_mann[grepl("SC_FT",DF$name)]<-"convtill_nores"
-DF$ov_mann[grepl("CS_RT",DF$name)]<-"rotTill_nores"
-DF$ov_mann[grepl("SC_RT",DF$name)]<-"rotTill_nores"
-DF$ov_mann[grepl("CS_RotT",DF$name)]<-"rotTill_nores"
-DF$ov_mann[grepl("SC_RotT",DF$name)]<-"rotTill_nores"
-DF$ov_mann[grepl("CS_NT",DF$name)]<-"notill_0.5-1res"
-DF$ov_mann[grepl("SC_NT",DF$name)]<-"notill_0.5-1res"
-DF$ov_mann[grepl("CS_NTcc",DF$name)]<-"notill_0.5-1res"
-DF$ov_mann[grepl("SC_NTcc",DF$name)]<-"notill_0.5-1res"
-DF$ov_mann[grepl("CSWS",DF$name)]<-"notill_0.5-1res"
-DF$ov_mann[grepl("CSWcc",DF$name)]<-"notill_0.5-1res"
+basedf$ov_mann[grepl("CS_FT",basedf$name)]<-"convtill_nores"
+basedf$ov_mann[grepl("SC_FT",basedf$name)]<-"convtill_nores"
+basedf$ov_mann[grepl("CS_RT",basedf$name)]<-"rotTill_nores"
+basedf$ov_mann[grepl("SC_RT",basedf$name)]<-"rotTill_nores"
+basedf$ov_mann[grepl("CS_RotT",basedf$name)]<-"rotTill_nores"
+basedf$ov_mann[grepl("SC_RotT",basedf$name)]<-"rotTill_nores"
+basedf$ov_mann[grepl("CS_NT",basedf$name)]<-"notill_0.5-1res"
+basedf$ov_mann[grepl("SC_NT",basedf$name)]<-"notill_0.5-1res"
+basedf$ov_mann[grepl("CS_NTcc",basedf$name)]<-"notill_0.5-1res"
+basedf$ov_mann[grepl("SC_NTcc",basedf$name)]<-"notill_0.5-1res"
+basedf$ov_mann[grepl("CSWS",basedf$name)]<-"notill_0.5-1res"
+basedf$ov_mann[grepl("CSWcc",basedf$name)]<-"notill_0.5-1res"
 
 #tile
-DF$tile<-"null"
+basedf$tile<-"null"
 
 #sep
-DF$sep<-"null"
+basedf$sep<-"null"
 
 #vfs
-DF$vfs<-"null"
+basedf$vfs<-"null"
 
 #grww
-DF$grww<-"null"
+basedf$grww<-"null"
 
 #bmp
-DF$bmp<-"null"
+basedf$bmp<-"null"
 
 ################## Add tile ################################
 
-tile<-DF
+tile<-basedf
 tile$tile<-"OWC_drain"
 tile$name<-paste0(tile$name, "_tile")
 
 ################## Add gww to tile and non tile ############
 
 #non tile gww
-grww_hi<-DF
+grww_hi<-basedf
 grww_hi$grww<-"grwway_high"
 grww_hi$name<-paste0(grww_hi$name,"_GW_HS")
 
-grww_med<-DF
+grww_med<-basedf
 grww_med$grww<-"grwway_med"
 grww_med$name<-paste0(grww_med$name,"_GW_MS")
 
-grww_low<-DF
+grww_low<-basedf
 grww_low$grww<-"grwway_low"
 grww_low$name<-paste0(grww_low$name,"_GW_LS")
 
@@ -125,7 +126,7 @@ grww_low_tile$name<-paste0(grww_low_tile$name,"_GW_LS")
 ############### Add buffers to tile and non tile ##########
 
 #non tile buffer
-vfs<-DF
+vfs<-basedf
 vfs$vfs<-"field_border"
 vfs$name<-paste0(vfs$name,"_B")
 
@@ -134,6 +135,51 @@ vfs_tile<-tile
 vfs_tile$vfs<-"field_border"
 vfs_tile$name<-paste0(vfs_tile$name,"_B")
 
-write_to_lum<-rbind(DF,tile,grww_hi,grww_med,grww_low,grww_hi_tile,grww_med_tile,grww_low_tile,vfs,vfs_tile)
+DF<-rbind(basedf,tile,grww_hi,grww_med,grww_low,grww_hi_tile,grww_med_tile,grww_low_tile,vfs,vfs_tile)
 
 ############# Read and overwrite landuse.lum ###############
+#read past first 6 lines and keep--urban and generic SWAT inputs
+file_dir<-file.path(here("Baseline","landuse.lum"))
+tmp <- file(file_dir)
+open(tmp, "r") #read
+
+#read past headerline and save to rewrite the file
+topOfFile<-readLines(tmp, n = 8) 
+
+spaceOutput<-function(data,nspaces){
+  
+  newData<-paste0(str_dup(" ",(nspaces-nchar(data))),data)
+  return(newData)
+  
+}
+
+spaceOutput_spacesecond<-function(data,nspaces){
+  
+  newData<-paste0(data,str_dup(" ",(nspaces-nchar(data))))
+  return(newData)
+  
+}
+
+#space the first two columns appropriately
+DF[[1]]<-spaceOutput_spacesecond(DF[[1]],20) 
+
+#all other columns have 14 spaces
+for (i in c(2:length(DF))){
+  
+  DF[[i]] <- spaceOutput(DF[[i]],18)
+  
+  
+}
+
+#had issues removing the file, so instead I opened and wiped the file of 
+#its contents and then wrote new data
+close( file( file_dir, open="w" ) ) 
+
+sink(file_dir, type=c("output"), append = T)
+
+write(c(topOfFile),file_dir,sep = "\n",append=T)
+
+write(c(paste0(DF[[1]],DF[[2]],DF[[3]],DF[[4]],DF[[5]],DF[[6]],DF[[7]],DF[[8]],DF[[9]],DF[[10]],
+               DF[[11]],DF[[12]],DF[[13]],DF[[14]])),file_dir,sep="\n",append=T)
+
+sink()
